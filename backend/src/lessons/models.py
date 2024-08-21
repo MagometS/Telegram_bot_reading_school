e50723ca637f3c1719ca62fde2735a5471234511
MagometS <magik200219@gmail.com>
2024-08-21 16:44:38 +0300

    feat: добавить модель урока

--- backend/src/lessons/models.py
+++ backend/src/lessons/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=250,
+        verbose_name='Название'
+    )
+    link = models.URLField(
+        max_length=250,
+        verbose_name='Ссылка'
+    )
+
+
+    def __str__(self) -> str:
+        return 'Урок:' + self.title
